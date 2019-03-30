6db47bd2b280b3b888534b91d0bc759cd4988a48
IvNSml <tweetshal@yandex.ru>
2019-03-31 01:33:10 +0300

    Its really makes me feel ashamed. I hope that's NOT all

--- jsonGetter.go
+++ jsonGetter.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
-	$END$
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"time"
+)
+
+type Person struct {
+	Name         string
+	Age          int
+	Nicknames    []string
+	Achievements map[int]string
 }
+type SayMyName interface {
+	SayName()
+}
+
+func(p Person) SayName()  {
+	fmt.Println("My name is ", p.Name)
+}
+func main() {
+	Vasya := CreatePerson()
+	rand.Seed(time.Now().Unix())
+	fmt.Println(Vasya.Nicknames[rand.Int()%len(Vasya.Nicknames)])
+	Vasya.SayName()
+}
+
+func CreatePerson() Person{
+	var text, err = ioutil.ReadFile("/home/ivan/Документы/homework.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var man Person
+	err =json.Unmarshal(text, &man)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return man
+}
+
